169696889af8453b81bafd895d09b341645dd9fb
Dima <dima.tolmachev@gmail.com>
2021-06-30 00:34:00 +0400

    update

--- options/auth/auth.js
+++ options/auth/auth.js
@@ -3,22 +3,21 @@ chrome.storage.sync.get('license', function (key) {
 
     $.getJSON('https://ipapi.co/json/', function (data) {
         var ipAddress = data.ip;
+        setTimeout(function () {
+            fetch(`https://degosh.com/shelterPlus-extension/${$("#key").val()}/${ipAddress}`).then(function (response) {
+                return response.text();
+            }).then(function (html) {
+                if (html == "OK") {
+                    alert("Good key!");
+                    //window.location.replace('/options/profiles/profiles.html');
+                } else {
+                    alert("Bad key!");
+                }
+            }).catch(function (err) {
+                console.log('Something went wrong', err);
+            });
+        }, 500);
     });
-
-    setTimeout(function () {
-        fetch(`https://degosh.com/shelterPlus-extension/${$("#key").val()}/${ipAddress}`).then(function (response) {
-            return response.text();
-        }).then(function (html) {
-            if (html == "OK") {
-                alert("Good key!");
-                //window.location.replace('/options/profiles/profiles.html');
-            } else {
-                alert("Bad key!");
-            }
-        }).catch(function (err) {
-            console.warn('Something went wrong', err);
-        });
-    }, 200);
 });
 
 $(document).ready(function () {
