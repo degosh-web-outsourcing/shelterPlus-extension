5b0c1425830779725e576d7366cf681a62cebd71
Dima <dima.tolmachev@gmail.com>
2021-06-29 22:26:14 +0400

    shelter plus gate created

--- options/auth/auth.js
+++ options/auth/auth.js
@@ -1,19 +1,23 @@
 chrome.storage.sync.get('license', function (key) {
     $("#key").val(key.license);
 
-    setTimeout(function() {
-        fetch(`https://degosh.com/activekey.php?key=` + $("#key").val()).then(function (response) {
-                return response.text();
-            }).then(function (html) {
-                if (html == "OK") {
-                    alert("Good key!")
-                    //window.location.replace('/options/profiles/profiles.html');
-                } else {
-                    alert("Bad key!")
-                }
-            }).catch(function (err) {
-                console.warn('Something went wrong', err);
-            });
+    $.getJSON('https://ipapi.co/json/', function (data) {
+        var ipAddress = data.ip;
+    });
+
+    setTimeout(function () {
+        fetch(`https://degosh.com/shelterPlus-extension/${$("#key").val()}/${ipAddress}`).then(function (response) {
+            return response.text();
+        }).then(function (html) {
+            if (html == "OK") {
+                alert("Good key!");
+                //window.location.replace('/options/profiles/profiles.html');
+            } else {
+                alert("Bad key!");
+            }
+        }).catch(function (err) {
+            console.warn('Something went wrong', err);
+        });
     }, 200);
 });
 
