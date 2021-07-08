e5a9069af7ebfda3e74cc75a7ab2b833642d2add
Dima <dima.tolmachev@gmail.com>
2021-07-08 15:17:20 +0400

    upd

--- options/profiles/profiles.js
+++ options/profiles/profiles.js
@@ -1,21 +1,26 @@
 var profile = { 'fname': null, 'sname': null, 'email': null, 'phone': null, 'country': null, 'state': null, 'city': null, 'zip': null, 'address1': null, 'address2': null, 'apt': null, 'cardNumber': null, 'expdate': null, 'cvv': null, 'bname': null, 'profileName': null, 'selected': null };
 var testProfile = { 'fname': "Игорь", 'sname': "Павлов", 'email': "test@example.com", 'phone': "9155553322", 'country': "Russia", 'state': "Moscow", 'city': "Москва", 'zip': "125310", 'address1': "Ул. Полянка, 25", 'address2': "подъезд 2", 'apt': "75", 'cardNumber': "4242 4242 4242 4242", 'expdate': "0230", 'cvv': "123", 'bname': "Игорь Павлов", 'profileName': "Test" };
 
-$(function() {
+$(function () {
     chrome.storage.local.get('profiles', function (list) {
         if (!list.profiles) {
             chrome.storage.local.set({ 'profiles': new Array() });
+        } else {
+            for (let i = 0; i < list.profiles.length; i++) {
+                pushProfile(list.profiles[i]);
+            }
         }
     });
 });
 
-$(function() {
+$(function () {
     $('#go').on('click', function () {
         Object.keys(profile).forEach(id => {
             profile[id] = $(`#${id}`).val();
         });
 
-        pushProfile()
+        pushProfile(profile);
+        createProfile(profile);
     });
 
     $('#test').on('click', function () {
@@ -59,29 +64,62 @@ $(function() {
     });
 });
 
-function pushProfile() {
-    let { profileName, country, cardNumber } = profile;
+function pushProfile(newProfile) {
+    let { profileName, country, cardNumber } = newProfile;
     let cardEnding = cardNumber[15] + cardNumber[16] + cardNumber[17] + cardNumber[18];
     var profilesPlace = document.getElementById("profilesList");
 
-    chrome.storage.local.get('profiles', function (list) {
-
-        profilesPlace.insertAdjacentHTML('beforeend', `
+    profilesPlace.insertAdjacentHTML('beforeend', `
             <div id="${profileName}" class="profile">
                 <a class="name">${profileName}</a>
                 <a class="country">${country}</a>
                 <a class="card">${cardEnding}</a>
-                <a class="trash">[X]</a>
+                <a id="${profileName}" class="trash">[X]</a>
             </div>
         `);
 
-        $('.trash').on('click', function () {
-            $(this).parent().remove();
-        });
+    $('.profile').on('click', function () {
+        var remote = $(this).attr('id');
+        setTimeout(function () {
+            chrome.storage.local.get('profiles', function (list) {
+                for (var i = 0; i < list.profiles.length; i++) {
+                    if (remote == list.profiles[i].profileName) {
+                        readProfile(list.profiles[i]);
+                    }
+                }
+            });
+        }, 100);
+    });
 
-        list.profiles.push(profile)
-        
+    $('.trash').on('click', function () {
+        var remote = $(this).attr('id');
+
+        setTimeout(function () {
+            chrome.storage.local.get('profiles', function (list) {
+                for (var i = 0; i < list.profiles.length; i++) {
+                    if (list.profiles[i].profileName == remote) {
+                        list.profiles.splice(i, 1);
+                        $(this).parent().remove();
+                        location.reload();
+                    }
+                }
+                chrome.storage.local.set({ 'profiles': list.profiles });
+            });
+        }, 100);
+    });
+}
+
+function createProfile(newProfile) {
+    chrome.storage.local.get('profiles', function (list) {
+        list.profiles.push(newProfile)
+
         chrome.storage.local.set({ 'profiles': list.profiles });
         console.log(list.profiles)
+    });
+}
+
+function readProfile(profile) {
+    Object.keys(profile).forEach(id => {
+        $(`#${id}`).val(profile[id]);
     });
 }
