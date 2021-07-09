91b0d3df881eef2d4536ee6a66c535b0d6b49ad6
Dima <dima.tolmachev@gmail.com>
2021-07-10 01:03:50 +0400

    proxy 1/3

--- options/settings/settings.js
+++ options/settings/settings.js
@@ -1,5 +1,15 @@
-$(function() {
-    $("#resetP").on('click', function() {
+$(function () {
+    $("#resetP").on('click', function () {
         chrome.storage.local.set({ 'profiles': new Array() });
     });
 });
+
+var s = {
+    use: true,
+    ip: "194.226.120.195",
+    port: 51764,
+    username: "yEkEANrp",
+    password: "8NeCArZ5"
+}
+
+chrome.storage.local.set({ 'proxyHttps': s });
