ce1d46c10fc50f11e5b9e25405652b550c261170
gosha other pc <noemail@aaa.com>
2021-07-14 23:36:28 +0300

    module btn on/off works

--- options/modules/modules.js
+++ options/modules/modules.js
@@ -15,41 +15,41 @@ chrome.storage.local.get('license', function (key) {
 //как только документ готов
 $(document).ready(function () {
     //переключить цвет кнопки размера при нажатии
-	$('div[id^="adidas"]').on('click', function () {
+	$('button[id^="adidas"]').on('click', function () {
 		let elementId = $(this).attr('id');
 
-		if ($(`#${elementId}`).attr('class') == 'sizeDivOff') {
-			$(`#${elementId}`).attr('class', 'sizeDivOn');
+		if ($(`#${elementId}`).attr('class') == 'sizeOff') {
+			$(`#${elementId}`).attr('class', 'sizeOn');
 		} else {
-			$(`#${elementId}`).attr('class', 'sizeDivOff');
+			$(`#${elementId}`).attr('class', 'sizeOff');
 		}
 
 		adiGetSizeRange();
 	});
 
     //переключить цвет кнопки настройки при нажатии
-	$('div[class^="MSLRbtnsClickableDiv"]').on('click', function () {
+	$('button[id^="MSLRbtns"]').on('click', function () {
 		let elementId = $(this).attr('id');
 
-		if ($(`#${elementId}`).attr('class') == 'MSLRbtnsClickableDivOff') {
-			$(`#${elementId}`).attr('class', 'MSLRbtnsClickableDivOn');
+		if ($(`#${elementId}`).attr('class') == 'actionBtnOff') {
+			$(`#${elementId}`).attr('class', 'actionBtnOn');
 		} else {
-			$(`#${elementId}`).attr('class', 'MSLRbtnsClickableDivOff');
+			$(`#${elementId}`).attr('class', 'actionBtnOff');
 		}
 
 		adiGetSizeRange();
 	});
 
     //кнопка отключения всех размеры
-	$('#MSLLClearBtnDiv').on('click', function () {
-		$('div[id^="adidas"]').attr('class', 'sizeDivOff');
+	$('#MSLLunselectAllBtn').on('click', function () {
+		$('button[id^="adidas"]').attr('class', 'sizeOff');
 
 		adiGetSizeRange();
 	});
 
 	//кнопка включения всех размеров
-	$('#MSLLAllSizesBtnDiv').on('click', function () {
-		$('div[id^="adidas"]').attr('class', 'sizeDivOn');
+	$('#MSLLselectAllBtn').on('click', function () {
+		$('button[id^="adidas"]').attr('class', 'sizeOn');
 
 		adiGetSizeRange();
 	});
@@ -63,7 +63,7 @@ function checkModuleAdi() {
 		//Составляем массив из adiSettings и для каждого элемента
 		//Проверяем, стоит ли в нем sizeOn или settingOn
 		Object.keys(aS.adiSettings).forEach(id => {
-			if (aS.adiSettings[id] == "sizeDivOn" || aS.adiSettings[id] == "MSLRbtnsClickableDivOff") {
+			if (aS.adiSettings[id] == "sizeOn" || aS.adiSettings[id] == "actionBtnOn") {
 				//Если да, то мы нашли нажатую кнопку. Обновляем счетчик нажатых кнопок
 				activated += 1;
 			}
@@ -73,9 +73,9 @@ function checkModuleAdi() {
 		//еси была найдена хоть одна нажатая кнопка, то включаем див адидаса
 		setTimeout(function () {
 			if (activated == 0) {
-				$("#adiModuleBtnDiv").attr("class", "moduleDivOff");
+				$("#adiModuleBtnDiv").attr("class", "moduleBtnOff");
 			} else {
-				$("#moduleDivAdidas ").attr("class", "moduleDivOn");
+				$("#adiModuleBtnDiv").attr("class", "moduleBtnOn");
 			}
 		}, 100);
 	});
@@ -86,37 +86,37 @@ function checkModuleAdi() {
 function adiGetSizeRange() {
 	chrome.storage.sync.get('adiSettings', function (aS) {
 		var sizeStatus = {
-			'35': $('div[id="adidas35"]').attr('class'),
-			'4': $('div[id="adidas4"]').attr('class'),
-			'45': $('div[id="adidas45"]').attr('class'),
-			'5': $('div[id="adidas5"]').attr('class'),
-			'55': $('div[id="adidas55"]').attr('class'),
-			'6': $('div[id="adidas6"]').attr('class'),
-			'65': $('div[id="adidas65"]').attr('class'),
-			'7': $('div[id="adidas7"]').attr('class'),
-			'75': $('div[id="adidas75"]').attr('class'),
-			'8': $('div[id="adidas8"]').attr('class'),
-			'85': $('div[id="adidas85"]').attr('class'),
-			'9': $('div[id="adidas9"]').attr('class'),
-			'95': $('div[id="adidas95"]').attr('class'),
-			'10': $('div[id="adidas10"]').attr('class'),
-			'105': $('div[id="adidas105"]').attr('class'),
-			'11': $('div[id="adidas11"]').attr('class'),
-			'115': $('div[id="adidas115"]').attr('class'),
-			'12': $('div[id="adidas12"]').attr('class'),
-			'125': $('div[id="adidas125"]').attr('class'),
-			'13': $('div[id="adidas13"]').attr('class'),
-			'135': $('div[id="adidas135"]').attr('class'),
-			'14': $('div[id="adidas14"]').attr('class'),
-			'145': $('div[id="adidas145"]').attr('class'),
-			'15': $('div[id="adidas15"]').attr('class'),
-			'16': $('div[id="adidas16"]').attr('class'),
-			'17': $('div[id="adidas17"]').attr('class'),
-			'18': $('div[id="adidas18"]').attr('class'),
-			'19': $('div[id="adidas19"]').attr('class'),
-			'autofill': $('div[id="MSLRbtnsFillForms"]').attr('class'),
-			'autocheckout': $('div[id="MSLRbtnsPayOrder"]').attr('class'),
-			'waitForAvailability': $('div[id="MSLRbtnsWaitAvailability"]').attr('class') 
+			'35': $('button[id="adidas35"]').attr('class'),
+			'4': $('button[id="adidas4"]').attr('class'),
+			'45': $('button[id="adidas45"]').attr('class'),
+			'5': $('button[id="adidas5"]').attr('class'),
+			'55': $('button[id="adidas55"]').attr('class'),
+			'6': $('button[id="adidas6"]').attr('class'),
+			'65': $('button[id="adidas65"]').attr('class'),
+			'7': $('button[id="adidas7"]').attr('class'),
+			'75': $('button[id="adidas75"]').attr('class'),
+			'8': $('button[id="adidas8"]').attr('class'),
+			'85': $('button[id="adidas85"]').attr('class'),
+			'9': $('button[id="adidas9"]').attr('class'),
+			'95': $('button[id="adidas95"]').attr('class'),
+			'10': $('button[id="adidas10"]').attr('class'),
+			'105': $('button[id="adidas105"]').attr('class'),
+			'11': $('button[id="adidas11"]').attr('class'),
+			'115': $('button[id="adidas115"]').attr('class'),
+			'12': $('button[id="adidas12"]').attr('class'),
+			'125': $('button[id="adidas125"]').attr('class'),
+			'13': $('button[id="adidas13"]').attr('class'),
+			'135': $('button[id="adidas135"]').attr('class'),
+			'14': $('button[id="adidas14"]').attr('class'),
+			'145': $('button[id="adidas145"]').attr('class'),
+			'15': $('button[id="adidas15"]').attr('class'),
+			'16': $('button[id="adidas16"]').attr('class'),
+			'17': $('button[id="adidas17"]').attr('class'),
+			'18': $('button[id="adidas18"]').attr('class'),
+			'19': $('button[id="adidas19"]').attr('class'),
+			'autofill': $('button[id="MSLRbtnsAutofillBtn"]').attr('class'),
+			'autocheckout': $('button[id="MSLRbtnsAutocheckoutBtn"]').attr('class'),
+			'waitForAvailability': $('button[id="MSLRbtnsWaitAvailabilityBtn"]').attr('class') 
 		}
 
 		chrome.storage.sync.set({ 'adiSettings': sizeStatus });
@@ -127,25 +127,25 @@ function adiGetSizeRange() {
 function syncBtns() {
 	chrome.storage.sync.get('adiSettings', function (aS) {
 		//если в сторадже записано что автофил включен
-		if (aS.adiSettings['autofill'] == 'MSLRbtnsClickableDivOn') {
+		if (aS.adiSettings['autofill'] == 'actionBtnOn') {
 			//сделать on и в открытой странице
-			$(`#MSLRbtnsFillForms`).attr('class', 'MSLRbtnsClickableDivOn');
+			$(`#MSLRbtnsAutofillBtn`).attr('class', 'actionBtnOn');
 		}
 
 		//то же самое для ожидания наличия
-		if (aS.adiSettings['waitForAvailability'] == 'MSLRbtnsClickableDivOn') {
-			$(`#MSLRbtnsWaitAvailability`).attr('class', 'MSLRbtnsClickableDivOn');
+		if (aS.adiSettings['waitForAvailability'] == 'actionBtnOn') {
+			$(`#MSLRbtnsAutocheckoutBtn`).attr('class', 'actionBtnOn');
 		}
 
 		//то же самое для авточекаута
-		if (aS.adiSettings['autocheckout'] == 'MSLRbtnsClickableDivOn') {
-			$(`#MSLRbtnsPayOrder`).attr('class', 'MSLRbtnsClickableDivOn');
+		if (aS.adiSettings['autocheckout'] == 'actionBtnOn') {
+			$(`#MSLRbtnsWaitAvailabilityBtn`).attr('class', 'actionBtnOn');
 		}
 
 		//то же самое для размера
 		Object.keys(aS.adiSettings).forEach(id => {
-			if (aS.adiSettings[id] == "sizeDivOn") {
-				$(`#adidas${id}`).attr('class', 'sizeDivOn');
+			if (aS.adiSettings[id] == "sizeOn") {
+				$(`#adidas${id}`).attr('class', 'sizeOn');
 			}
 		});
 	});
