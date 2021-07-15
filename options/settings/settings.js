f535d5e9fba76774d79f2573907cd5d01c1ad4a0
Dima <dima.tolmachev@gmail.com>
2021-07-15 15:56:22 +0400

    upd

--- options/settings/settings.js
+++ options/settings/settings.js
@@ -11,56 +11,13 @@ chrome.storage.local.get('license', function (key) {
         console.log('Something went wrong', err);
     });
 });
-
-//как только документ готов
-$(document).ready(function () {
-    //переключить цвет кнопки proxy по тыку
-	$('button[id="useProxyBtn"]').on('click', function () {
-		let elementId = $(this).attr('id');
-
-		if ($(`#${elementId}`).attr('class') == 'BtnOff') {
-			$(`#${elementId}`).attr('class', 'BtnOn');
-		} else {
-			$(`#${elementId}`).attr('class', 'BtnOff');
-		}
-
-        //записываем статус кнопки в chrome storage
-		SettingsSizeRange();
-	});
-});
-
-//перезаписывает статусы кнопок в переменную в chrome storage
-function SettingsSizeRange() {
-	chrome.storage.sync.get('Settings', function (Set) {
-		var btnsStatus = {
-			'proxyBtnClass': $('button[id="useProxyBtn"]').attr('class')
-		}
-
-		chrome.storage.sync.set({ 'Settings': btnsStatus});
-	});
-}
-
-//делает кнопки такими же, как записано в соответствующей переменной в chrome storage
-function syncBtns() {
-	chrome.storage.sync.get('Settings', function (Set) {
-		//если в сторадже записано что proxy включен
-		if (Set.Settings['proxyBtnClass'] == 'BtnOn') {
-			//сделать эту кнопку on и в открытой странице
-			$(`#proxyBtnClass`).attr('class', 'BtnOn');
-		}
-    });
-
-    //снова записываем статус этой кнопки
-	SettingsSizeRange();
-}
-    
-
+  
 $(function () {
-    $("#resetP").on('click', function () {
+    $("#resetProfilesBtn").on('click', function () {
         chrome.storage.local.set({ 'profiles': new Array() });
     });
 
-    $('#exit').on('click', function () {
+    $('#exitAppBtn').on('click', function () {
         chrome.storage.local.set({ 'license': null });
         window.location.href = "../auth/auth.html";
     });
@@ -72,7 +29,7 @@ $(function () {
             $('#proxyInput').val(sw.proxyHttps.proxyD);
 
             if (sw.proxyHttps.status) {
-                $('#useProxy').attr('class', 'proxyOn');
+                $('#useProxyBtn').attr('class', 'proxyOn');
             } else {
                 chrome.storage.local.set({ 'proxyHttps': { status: false, proxyD: sw.proxyHttps.proxyD } });
             }
@@ -85,16 +42,16 @@ $(function () {
             });
         });
 
-        $('#useProxy').on('click', function () {
+        $('#useProxyBtn').on('click', function () {
             var proxyData = $('#proxyInput').val();
             if (document.getElementById('proxyInput').value.length) {
-                if ($('#useProxy').attr('class') == 'proxyOff') {
-                    $('#useProxy').attr('class', 'proxyOn');
+                if ($('#useProxyBtn').attr('class') == 'proxyOff') {
+                    $('#useProxyBtn').attr('class', 'proxyOn');
                     chrome.storage.local.set({ 'proxyHttps': { status: true, proxyD: proxyData } });
                     chrome.tabs.create({ url: "https://degosh.com/" });
                     chrome.runtime.reload();
                 } else {
-                    $('#useProxy').attr('class', 'proxyOff');
+                    $('#useProxyBtn').attr('class', 'proxyOff');
                     chrome.storage.local.set({ 'proxyHttps': { status: false, proxyD: proxyData } });
                     chrome.tabs.create({ url: "https://degosh.com/" });
                     chrome.runtime.reload();
