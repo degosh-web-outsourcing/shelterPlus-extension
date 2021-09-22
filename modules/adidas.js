0594f6955231dec688382854054240a9817c5e9f
dima-tolmachev <dima.tolmachev@gmail.com>
2021-09-22 22:55:13 +0400

    adidas autocart fix

--- modules/adidas.js
+++ modules/adidas.js
@@ -83,7 +83,9 @@ chrome.storage.local.get('profiles', function (list) {
                             }
 
                             if ($('[data-auto-id="explicit-consent-checkbox"]:checked').length == 0 && window.location.href.includes("delivery")) {
-                                $('[type="checkbox"]')[1].click();
+                                if ($('[type="checkbox"]')[1]) {
+                                    $('[type="checkbox"]')[1].click();
+                                }
                             }
 
 
@@ -108,28 +110,70 @@ if (window.location.pathname.includes("yeezy")) {
                 if (Object.entries(aS.adiSettings)[i][1] == "sizeOn") {
                     if (Object.keys(aS.adiSettings)[i] == "35") {
                         positiveSizes.push("3.5");
+                        positiveSizes.push("35");
                     } else if (Object.keys(aS.adiSettings)[i] == "45") {
                         positiveSizes.push("4.5");
+                        positiveSizes.push("36");
                     } else if (Object.keys(aS.adiSettings)[i] == "55") {
                         positiveSizes.push("5.5");
+                        positiveSizes.push("37");
                     } else if (Object.keys(aS.adiSettings)[i] == "65") {
                         positiveSizes.push("6.5");
+                        positiveSizes.push("38");
                     } else if (Object.keys(aS.adiSettings)[i] == "75") {
                         positiveSizes.push("7.5");
+                        positiveSizes.push("39");
                     } else if (Object.keys(aS.adiSettings)[i] == "85") {
                         positiveSizes.push("8.5");
+                        positiveSizes.push("40.5");
                     } else if (Object.keys(aS.adiSettings)[i] == "95") {
                         positiveSizes.push("9.5");
+                        positiveSizes.push("42");
                     } else if (Object.keys(aS.adiSettings)[i] == "105") {
                         positiveSizes.push("10.5");
+                        positiveSizes.push("43");
                     } else if (Object.keys(aS.adiSettings)[i] == "115") {
                         positiveSizes.push("11.5");
+                        positiveSizes.push("44.5");
                     } else if (Object.keys(aS.adiSettings)[i] == "125") {
                         positiveSizes.push("12.5");
+                        positiveSizes.push("46");
                     } else if (Object.keys(aS.adiSettings)[i] == "135") {
                         positiveSizes.push("13.5");
+                        positiveSizes.push("47");
                     } else if (Object.keys(aS.adiSettings)[i] == "145") {
                         positiveSizes.push("14.5");
+                        positiveSizes.push("48.5");
+                    } else if (Object.keys(aS.adiSettings)[i] == "4") {
+                        positiveSizes.push("4");
+                        positiveSizes.push("35.5");
+                    } else if (Object.keys(aS.adiSettings)[i] == "5") {
+                        positiveSizes.push("5");
+                        positiveSizes.push("36.5");
+                    } else if (Object.keys(aS.adiSettings)[i] == "6") {
+                        positiveSizes.push("6");
+                        positiveSizes.push("37.5");
+                    } else if (Object.keys(aS.adiSettings)[i] == "7") {
+                        positiveSizes.push("7");
+                        positiveSizes.push("38.5");
+                    } else if (Object.keys(aS.adiSettings)[i] == "8") {
+                        positiveSizes.push("8");
+                        positiveSizes.push("40");
+                    } else if (Object.keys(aS.adiSettings)[i] == "9") {
+                        positiveSizes.push("9");
+                        positiveSizes.push("41");
+                    } else if (Object.keys(aS.adiSettings)[i] == "10") {
+                        positiveSizes.push("10");
+                        positiveSizes.push("4.5");
+                    } else if (Object.keys(aS.adiSettings)[i] == "11") {
+                        positiveSizes.push("11");
+                        positiveSizes.push("44");
+                    } else if (Object.keys(aS.adiSettings)[i] == "12") {
+                        positiveSizes.push("12");
+                        positiveSizes.push("45");
+                    } else if (Object.keys(aS.adiSettings)[i] == "13") {
+                        positiveSizes.push("13");
+                        positiveSizes.push("46.5");
                     } else {
                         positiveSizes.push(Object.keys(aS.adiSettings)[i]);
                     }
@@ -139,7 +183,7 @@ if (window.location.pathname.includes("yeezy")) {
     });
 
     let checkExist = setInterval(function () {
-        if ($('li:contains("UK")').length) {
+        if ($('li:contains("UK")').length || $('li:contains("RU")').length) {
             clearInterval(checkExist);
 
             setTimeout(function () {
@@ -212,8 +256,8 @@ if (window.location.origin.includes("yoomoney")) {
 
 function clickSize(array) {
     var size = array[Math.floor(Math.random() * array.length)];
-    if ($(`li:contains(" ${size} UK")`).length) {
-        $(`li:contains(" ${size} UK")`).click();
+    if ($(`li:contains("${size}")`).length) {
+        $(`li:contains("${size}")`).click();
         setTimeout(function () {
             $('[data-auto-id="button-add-to-bag"]').click();
         }, 1000);
