3d25b628e7a4db6ec090487eadb1cb54c357bc3b
dima-tolmachev <dima.tolmachev@gmail.com>
2021-09-23 01:23:55 +0400

    proxy switcher

--- options/settings/settings.js
+++ options/settings/settings.js
@@ -103,42 +103,90 @@ function download(filename, text) {
     document.body.removeChild(element); 
 }
 
-$(function () {
-    /*
-    chrome.storage.local.get('proxyHttps', function (sw) {
-        if (sw.proxyHttps) {
-            $('#proxyInput').val(sw.proxyHttps.proxyD);
+////
 
-            if (sw.proxyHttps.status) {
-                $('#useProxyBtn').attr('class', 'proxyOn');
-            } else {
-                chrome.storage.local.set({ 'proxyHttps': { status: false, proxyD: sw.proxyHttps.proxyD } });
-            }
+chrome.storage.local.get("proxy", function (data) {
+    if (data.proxy != undefined) {
+        $('#proxyInput').val(data.proxy.data);
+        if (data.proxy.switcher == true) {
+            $('#useProxyBtn').attr('class', 'proxyOn');
+            go(true);
         }
+    }
+});
 
-        $('#proxyInput').change(function () {
-            chrome.storage.local.get('proxyHttps', function (sw) {
-                var proxyData = $('#proxyInput').val();
-                chrome.storage.local.set({ 'proxyHttps': { status: sw.proxyHttps.status, proxyD: proxyData } });
-            });
-        });
-
-        $('#useProxyBtn').on('click', function () {
-            var proxyData = $('#proxyInput').val();
-            if (document.getElementById('proxyInput').value.length) {
-                if ($('#useProxyBtn').attr('class') == 'proxyOff') {
-                    $('#useProxyBtn').attr('class', 'proxyOn');
-                    chrome.storage.local.set({ 'proxyHttps': { status: true, proxyD: proxyData } });
-                    chrome.tabs.create({ url: "https://degosh.com/" });
-                    chrome.runtime.reload();
-                } else {
-                    $('#useProxyBtn').attr('class', 'proxyOff');
-                    chrome.storage.local.set({ 'proxyHttps': { status: false, proxyD: proxyData } });
-                    chrome.tabs.create({ url: "https://degosh.com/" });
-                    chrome.runtime.reload();
-                }
+$(function () {
+    $(document).on('change', '#proxyInput', function () {
+        chrome.storage.local.get("proxy", function (data) {
+            var status = {
+                'data': $('#proxyInput').val(),
+                'switcher': false
             }
+            $('#useProxyBtn').attr('class', "proxyOff");
+            chrome.storage.local.set({ 'proxy': status });
         });
     });
-    */
+
+    $('#useProxyBtn').on('click', function () {
+        var status = new Object();
+
+        if ($('#useProxyBtn').attr('class') == "proxyOn") {
+            $('#useProxyBtn').attr('class', "proxyOff");
+            status = {
+                'data': $('#proxyInput').val(),
+                'switcher': false
+            }
+            go(false);
+        } else {
+            status = {
+                'data': $('#proxyInput').val(),
+                'switcher': true
+            }
+            $('#useProxyBtn').attr('class', 'proxyOn');
+            go(true);
+        }
+        chrome.storage.local.set({ 'proxy': status });
+    });
 });
+
+function go(b) {
+    chrome.storage.local.get("proxy", function (data) {
+        pr = data.proxy.data.split(':');
+        var config = {
+            mode: "direct"
+        };
+
+        if (b == true) {
+            var config = {
+                mode: "fixed_servers",
+                rules: {
+                    singleProxy: {
+                        scheme: "http",
+                        host: pr[0],
+                        port: parseInt(pr[1])
+                    },
+                    bypassList: ["degosh.com", "dashboard.degosh.com"]
+                }
+            };
+        }
+
+        setTimeout(() => {
+            chrome.proxy.settings.set(
+                { value: config, scope: 'regular' },
+                function () { });
+
+            if (b == true) {
+                chrome.webRequest.onAuthRequired.addListener(
+                    function (details, callbackFn) {
+                        console.log("onAuthRequired!", details, callbackFn);
+                        callbackFn({
+                            authCredentials: { username: pr[2], password: pr[3] }
+                        });
+                    },
+                    { urls: ["<all_urls>"] },
+                    ['asyncBlocking']
+                );
+            }
+        }, 250);
+    });
+}
